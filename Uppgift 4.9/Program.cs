10cc59a453904f975b8db3c69fa7ef09e4ce20c2
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-23 15:09:11 +0200

    Add project files.

---
+++ Uppgift 4.9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bestäm sidlängd för en triangel");
+            int längd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= längd; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
